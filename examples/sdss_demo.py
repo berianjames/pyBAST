Some fixes and output tweaks to regression uncertainty calculation.

examples/sdss_demo.py
@@ -42,22 +42,22 @@ print D.amp
 print 'Single point regression'
 xy = np.array([0,0])
 R = D.regression(xy)
-print R
+print R[0]
 
 # Regression of a single distribution
 print 'Single distribution regression'
 BV = pyBA.Bivarg()
 R = D.regression(BV)
-print R
+print R[0]
 
 # Regression of an array of points
 print 'Multiple point regression'
 xy = 100*np.random.rand(10,2) - 50
 R = D.regression(xy)
-print R
+print R[0]
 
 # Regression of an array of distributions
 print 'Multiple distribution regression'
 BV = [pyBA.Bivarg(mu=xyi, sigma=np.random.rand(2)) for xyi in xy]
 R = D.regression(BV)
-print R
+print R[0]

pyBA/background.py
@@ -104,10 +104,10 @@ def MAP(M,N,mu0=Bgmap().mu,prior=Bgmap(),norm_approx=True):
         try:
             np.linalg.cholesky(sigma)
         except np.linalg.linalg.LinAlgError:
-            # Flip sign of eigenvalues. This is different to the method of Higham (2002) but
-            #  performs well for these matrices.
+            # Zero negative eigenvalues. This is the method of Higham (2002).
             E, V = np.linalg.eigh(sigma)
-            sigma = V.dot(np.diag(np.abs(E)).dot(V.T))
+            E[E<0] = 1e-12
+            sigma = V.dot(np.diag(E).dot(V.T))
         
         return Bgmap( mu=ML, sigma=sigma )
 

pyBA/classes.py
@@ -90,35 +90,48 @@ class Bgmap:
         Input: xy, an array of n bivargs, at whose centres the uncertainty contribution will be calculated
         Output: S_P, an array of n 2x2 covariance matrices, one for each input location. 
         """
+        from pyBA.distortion import astrometry_mean as mu_transform
 
         # Sample background mapping distribution
-        N = 100 # Free parameter: how many samples to draw when evaluating covariance?
-        P = self.sample(n=N)
+        N = 1000 # Free parameter: how many samples to draw when evaluating covariance?
+        #P = self.sample(n=N)
 
         # If the rotation and scaling parameters are sufficiently close to identity, then the variance contribution
         #  from the background mapping will be independent of location in the plane (essentially, all the contribution
         #  is from the translation). In this case, the covariance contribution need only be computed once.
-        tol = 0.005 # Threshold for deciding whether variance contribution is independent of location in the plane
+        tol = 1 # Threshold for deciding whether variance contribution is independent of location in the plane
 
         # To check whether the variance contribution is location independent, look at how much the rotation and
         #  scaling parameters differ from their identity values.
-        theta_dep = self.mu[2] / self.sigma[2,2]
-        L1_dep = (1 - self.mu[5]) / self.sigma[5,5]
-        L2_dep = (1 - self.mu[6]) / self.sigma[6,6]
+        theta_dep = np.abs(self.mu[2]) / np.sqrt(self.sigma[2,2])
+        L1_dep = np.abs(1 - self.mu[5]) / np.sqrt(self.sigma[5,5])
+        L2_dep = np.abs(1 - self.mu[6]) / np.sqrt(self.sigma[6,6])
 
-        if theta_dep < tol and L1_dep < tol and L2_dep < tol:
-            # Variance from background mapping is approximately independent of location.
+        # If rotation parameter is very close to zero, the centre of rotation will be unconstrained. Numerically,
+        #  it is better to set the centre of rotation to zero in this case.
+        if theta_dep < tol:
+            self.mu[3:4] = 0
+            for i in (3,4):
+                self.sigma[i,:] = 0
+                self.sigma[:,i] = 0
+                self.sigma[i,i] = self.sigma[2,2]
 
-            # Compute covariance contribution for a single object
-            s_P = np.cov([xy[0].transform(p).mu for p in P], rowvar=0)
+#        if theta_dep < tol and L1_dep < tol and L2_dep < tol:
+#            # Variance from background mapping is approximately independent of location.
+#                
+#            # Compute covariance contribution for a single object
+#            s_P = np.cov([xy[0].transform(p).mu for p in self.sample(N)], rowvar=0)
+#
+#            # And stack that result for all the input object locations
+#            S_P = np.array([s_P for i in range(len(xy))])
+#
+#        else:
+#            # Otherwise, for each input object location, compute the variance of
+#            #  the transformed centres across all sampled transformations.
+#            S_P = np.array([np.cov([o.transform(p).mu for p in self.sample(N)], rowvar=0) for o in xy])
+#            #S_P = np.array([np.cov([mu_transform(o.mu,p) for p in self.sample(N)], rowvar=0) for o in xy])
 
-            # And stack that result for all the input object locations
-            S_P = np.array([s_P for i in range(len(xy))])
-
-        else:
-            # Otherwise, for each input object location, compute the variance of
-            #  the transformed centres across all sampled transformations.
-            S_P = np.array([np.cov([o.transform(p).mu for p in P], rowvar=0) for o in xy])
+        S_P = np.array([np.cov([o.transform(p).mu for p in self.sample(N)], rowvar=0) for o in xy])
 
         return S_P
         
@@ -195,14 +208,20 @@ class Bivarg:
     def __repr__(self):
         """ Print beautiful representation of bivarg object.
         """
-        reprstr = str(self.mu) + ' ' + str(self.sigma.ravel()[np.array([0, 3, 1])])
+        reprstr = str(self.mu) + ' ' + str(self.sigma.ravel())
         return reprstr
 
     def __str__(self):
         """ Print more verbose representation of bivarg."""
+        
+        if self.point == True:
+            rho_str = '---'
+        else:
+            rho_str = str(self.sigma[0,1] / np.sqrt(self.sigma[0,0]*self.sigma[1,1]))
+
         strstr = 'mu: ' + str(self.mu) + ', ' + \
             '[sxx syy]: ' + str(np.sqrt(self.sigma.ravel()[np.array([0,3])])) + ', ' + \
-            'rho_xy: ' + str(self.sigma[0,1] / np.sqrt(self.sigma[0,0]*self.sigma[1,1]))
+            'rho_xy: ' + rho_str
         return strstr
 
     def transform(self,P=Bgmap()):
@@ -481,4 +500,4 @@ class Amap:
         # Construct output array of Bivargs
         O = np.array([ Bivarg(mu=munew[i], sigma=sigmanew[i]) for i in range(len(R)) ])
 
-        return O
+        return O, S_gp, S_P

pyBA/distortion.py
@@ -36,17 +36,25 @@ def astrometry_cov(d2,scale=100.,amp=np.eye(2),var=None):
 
     return C
 
-def astrometry_mean(xy, T=Bgmap()):
+def astrometry_mean(xy, P=Bgmap()):
     """Mean functions for the gaussian process
     astrometric solution. Takes affine transformation
     and applies it to n x 2 vector of points."""
 
     # Prep inputs
-    dmu = T.mu[0:2]
-    theta = T.mu[2]
-    d0 = T.mu[3:5]
-    L = T.mu[5:7]
-    
+    if P.__class__.__name__ == 'Bgmap':
+        dmu = P.mu[0:2]
+        theta = P.mu[2]
+        d0 = P.mu[3:5]
+        L = P.mu[5:7]    
+    elif P.__class__.__name__ == 'ndarray':
+        dmu = P[0:2]
+        theta = P[2]
+        d0 = P[3:5]
+        L = P[5:7]
+    else:
+        raise TypeError('Argument to background mapping transform should be a Bgmap object or a 7-vector of parameters.')
+
     U = np.squeeze(np.array([ [np.cos(theta),-np.sin(theta)],
                               [np.sin(theta), np.cos(theta)] ]))
         
@@ -169,17 +177,23 @@ def optimise_HP(A, B, P, HP0):
         # Get first term of loglikelihood expression (y * (1/C) * y.T)
         # Do computation using Cholesky decomposition
         try:
+            
             U, luflag = cho_factor(C)
+            
+        except LinAlgError:
+
+            # Matrix is not positive semi-definite, so replace it with the 
+            #  positive semi-definite matrix that is nearest in the Frobenius norm
+
+            E, EV = eigh(C)
+            E[E<0] = 1e-12
+            U, luflag = cho_factor(EV.dot(np.diag(Ep)).dot(EV.T))
+            
+        finally:
+
             x2 = cho_solve((U, luflag), dxy)
             L1 = dxy.dot(x2)
-        except LinAlgError:
-            #print "EV tweak"
-            E, EV = eigh(C)
-            Ep = 1/E
-            Ep[abs(Ep)>1e5] = 0
-            Ci = EV.T.dot(np.diag(Ep)).dot(EV)
-            L1 = Ci.dot(dxy)
-                    
+
         # Get second term of loglikelihood expression (log det C)
         sign, L2 = slogdet(C)
 